334a4c32dcc3076e42cac2415fd5a89ada0ced1d
Mateo Davis <mateo.cohen.davis@gmail.com>
2015-10-11 14:18:59 -0400

    2 birds on background

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,15 @@
-// Replace this with your code...
+function init() {
+    var img = new Image();
+    var img2 = new Image();
+    img.addEventListener("load", function() {
+        game_canvas = document.getElementById("game_canvas");
+        var ctx = game_canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        i = 0;
+            ctx.drawImage(img2, 37, 119, 43, 30, 70, 60, 43, 30);
+            ctx.drawImage(img2, 119, 118, 43, 30, 120, 40, 43, 30);
+            i++;
+    }, false);
+    img.src = 'duckhunt-background.gif';
+    img2.src = 'duckhunt_various_sheet.png';
+}
