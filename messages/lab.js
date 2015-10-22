484e101dc60a843ca8da484f679555eebe1cc9a9
Mateo Davis <mateo.cohen.davis@gmail.com>
2015-10-22 19:16:54 -0400

    removed console.logs

--- messages/lab.js
+++ messages/lab.js
@@ -5,7 +5,6 @@ function parse () {
     myRequest.open("get", "http://messagehub.herokuapp.com/messages.json", "true");
     myRequest.send();
     myRequest.onreadystatechange = function() {
-        console.log(myRequest.readyState);
         if (myRequest.readyState == 4 && myRequest.status == 200) {
             text = JSON.parse(myRequest.responseText);
             for(var i = 0; i < 2; i++) {
